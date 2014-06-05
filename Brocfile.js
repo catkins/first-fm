cc10ccf2c2ad07bd4bfa2d349318344d6aa49895
Chris Atkins <christopherlionelatkins@gmail.com>
2014-06-05 22:04:46 +1000

    added bootstrap

--- Brocfile.js
+++ Brocfile.js
@@ -16,6 +16,10 @@ var app = new EmberApp({
 // Use this to add additional libraries to the generated output files.
 app.import('vendor/ember-data/ember-data.js');
 
+// bootstrap
+app.import('vendor/bootstrap/dist/js/bootstrap.js')
+app.import('vendor/bootstrap/dist/css/bootstrap.css')
+
 // If the library that you are including contains AMD or ES6 modules that
 // you would like to import into your application please specify an
 // object with the list of modules as keys along with the exports of each
