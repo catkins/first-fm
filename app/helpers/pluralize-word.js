740f7fefeaa7be71b824be9b32c8451000ef28d7
Chris Atkins <christopherlionelatkins@gmail.com>
2014-06-06 03:51:14 +1000

    Added pluralise word helper - adapted from https://coderwall.com/p/ryo_3w

---
+++ app/helpers/pluralize-word.js
@@ -0,0 +1,8 @@
+export default Ember.Handlebars.makeBoundHelper(function(number, options) {
+  var phraseMatch = ( options.hash.phrase || '{|s}' ).match( /(.*?)\{(.*?)\|(.*?)\}/ );
+  Ember.assert( 'The optional "phrase" hash for {{pluralize-word}} should be formatted as <phrase to pluralize>{<singular ending>|<plural ending>}', phraseMatch );
+  var word = phraseMatch[ 1 ],
+      singular = word + phraseMatch[ 2 ],
+      plural = word + phraseMatch[ 3 ];
+  return number === 1 ? singular : plural;
+});
