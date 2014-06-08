531d14ad3c052e0643afc7b8b2f486a0a2cf3d30
Chris Atkins <christopherlionelatkins@gmail.com>
2014-06-08 17:01:56 +1000

    added search for tracks and albums

--- app/services/spotify.js
+++ app/services/spotify.js
@@ -1,18 +1,51 @@
 var SpotifyService = Ember.Object.extend({
 
+  artworkUrlCache: {},
+  webServicePath: 'http://ws.spotify.com/search/1/',
+  artworkPath: 'https://embed.spotify.com/oembed/?callback=?',
+
   searchArtists: function(query) {
-    return $.getJSON('http://ws.spotify.com/search/1/artist.json', { q: query })
+    return this.querySearchService('artist', query)
             .then(function(response) {
               return response.artists;
             });
   },
 
   searchAlbums: function(query) {
-    // TODO
+    return this.querySearchService('album', query)
+            .then(function(response) {
+              return response.albums;
+            });
   },
 
   searchTracks: function(query) {
-    // TODO
+    return this.querySearchService('track', query)
+            .then(function(response) {
+              return response.tracks;
+            });
+  },
+
+  getArtwork: function(spotifyId) {
+    var artworkUrlCache = this.artworkUrlCache;
+
+    if (! artworkUrlCache[spotifyId]) {
+      artworkUrlCache[spotifyId] = this.queryEmbedService(spotifyId)
+         .then(function(response) {
+           var url = response.thumbnail_url;
+           return url;
+         }.bind(this));
+
+    }
+
+    return DS.PromiseObject.create({ promise: artworkUrlCache[spotifyId]});
+  },
+
+  querySearchService: function(type, query) {
+    return $.getJSON(this.webServicePath + type + '.json', { q: query });
+  },
+
+  queryEmbedService: function(spotifyId) {
+    return $.getJSON(this.artworkPath, { url: spotifyId });
   }
 
 });
