c92318127417b87492ed2d0c58783468db9b1c26
Chris Atkins <christopherlionelatkins@gmail.com>
2014-06-09 20:05:04 +1000

    added artwork property to albums and did a little bit of design on the album page

--- app/controllers/album.js
+++ app/controllers/album.js
@@ -1,4 +1,15 @@
 var AlbumController = Ember.ObjectController.extend({
+  placeholderImage: 'http://placehold.it/250x250&text=loading...',
+
+  artworkUrlWithDefault: function() {
+    return this.get('artworkUrl.content') || this.placeholderImage;
+  }.property('artworkUrl.content', 'artworkUrl'),
+
+  artworkUrl: function() {
+    var spotifyId = this.get('href');
+    var promise = this.get('spotify').getArtwork(spotifyId);
+    return DS.PromiseObject.create({ promise: promise});
+  }.property('href'),
 
 });
 
--- app/controllers/track-item.js
+++ app/controllers/track-item.js
@@ -1,7 +1,12 @@
 var TrackItemController = Ember.ObjectController.extend({
   trackNumber: function() {
     return this.get('model.track-number');
-  }.property('model')
+  }.property('model'),
+
+  duration: function() {
+    var duration = moment.duration(this.get('length'), 'seconds');
+    return duration.minutes() + ':' + duration.seconds();
+  }.property('length')
 });
 
 export default TrackItemController;
